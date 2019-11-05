c084e9ea2d2bb904276553cd8e9dc637b410cb58
Raisa <raisaorthy@gmail.com>
2019-11-05 19:56:39 +0600

    solved combined issue

---
+++ final_convert.py
@@ -0,0 +1,96 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+data=pd.read_csv('sample_cnn1.csv')
+data.replace(1,'automobile', inplace=True)
+data.replace(2,'bird', inplace=True)
+data.replace(3,'cat', inplace=True)
+data.replace(4,'deer', inplace=True)
+data.replace(5,'dog', inplace=True)
+data.replace(6,'frog', inplace=True)
+data.replace(7,'horse', inplace=True)
+data.replace(8,'ship', inplace=True)
+data.replace(9,'truck', inplace=True)
+data.replace(0,'airplane', inplace=True)
+data.to_csv('result/result1.csv',header=True,index=False)
+
+data=pd.read_csv('sample_cnn2.csv')
+data['id']=data['id']+50000
+data.replace(1,'automobile', inplace=True)
+data.replace(2,'bird', inplace=True)
+data.replace(3,'cat', inplace=True)
+data.replace(4,'deer', inplace=True)
+data.replace(5,'dog', inplace=True)
+data.replace(6,'frog', inplace=True)
+data.replace(7,'horse', inplace=True)
+data.replace(8,'ship', inplace=True)
+data.replace(9,'truck', inplace=True)
+data.replace(0,'airplane', inplace=True)
+data.to_csv('result/result2.csv',header=True,index=False)
+
+data=pd.read_csv('sample_cnn3.csv')
+data['id']=data['id']+100000
+data.replace(1,'automobile', inplace=True)
+data.replace(2,'bird', inplace=True)
+data.replace(3,'cat', inplace=True)
+data.replace(4,'deer', inplace=True)
+data.replace(5,'dog', inplace=True)
+data.replace(6,'frog', inplace=True)
+data.replace(7,'horse', inplace=True)
+data.replace(8,'ship', inplace=True)
+data.replace(9,'truck', inplace=True)
+data.replace(0,'airplane', inplace=True)
+data.to_csv('result/result3.csv',header=True,index=False)
+
+data=pd.read_csv('sample_cnn4.csv')
+data['id']=data['id']+150000
+data.replace(1,'automobile', inplace=True)
+data.replace(2,'bird', inplace=True)
+data.replace(3,'cat', inplace=True)
+data.replace(4,'deer', inplace=True)
+data.replace(5,'dog', inplace=True)
+data.replace(6,'frog', inplace=True)
+data.replace(7,'horse', inplace=True)
+data.replace(8,'ship', inplace=True)
+data.replace(9,'truck', inplace=True)
+data.replace(0,'airplane', inplace=True)
+data.to_csv('result/result4.csv',header=True,index=False)
+
+data=pd.read_csv('sample_cnn5.csv')
+data['id']=data['id']+200000
+data.replace(1,'automobile', inplace=True)
+data.replace(2,'bird', inplace=True)
+data.replace(3,'cat', inplace=True)
+data.replace(4,'deer', inplace=True)
+data.replace(5,'dog', inplace=True)
+data.replace(6,'frog', inplace=True)
+data.replace(7,'horse', inplace=True)
+data.replace(8,'ship', inplace=True)
+data.replace(9,'truck', inplace=True)
+data.replace(0,'airplane', inplace=True)
+data.to_csv('result/result5.csv',header=True,index=False)
+
+"""data=pd.read_csv('sample_cnn6.csv')
+data['id']=data['id']+250000
+data.replace(1,'automobile', inplace=True)
+data.replace(2,'bird', inplace=True)
+data.replace(3,'cat', inplace=True)
+data.replace(4,'deer', inplace=True)
+data.replace(5,'dog', inplace=True)
+data.replace(6,'frog', inplace=True)
+data.replace(7,'horse', inplace=True)
+data.replace(8,'ship', inplace=True)
+data.replace(9,'truck', inplace=True)
+data.replace(0,'airplane', inplace=True)
+data.to_csv('result/result6.csv',header=True,index=False)"""
+
+#way to combine multiple csv file
+
+os.chdir("result")
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+#export to csv
+combined_csv.to_csv( "final_csv.csv", index=False, encoding='utf-8-sig')
