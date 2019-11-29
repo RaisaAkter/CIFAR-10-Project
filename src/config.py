c06d404fb82cabea3726280e28cdf10aa6e805d4
Raisa <raisaorthy@gmail.com>
2019-11-29 09:30:41 +0600

    add description

--- src/config.py
+++ src/config.py
@@ -1,12 +1,19 @@
+'''
+Author: Raisa
+Date: 29-11-19
+'''
+
 import os
-nb_train=0
-nb_test=0
+nb_train=50000
+nb_test=300000
 img_size=28
 img_channel=3
 img_shape=(img_size,img_size,img_channel)
 
-lr_1=0.001 
-lr_2=0.0001
+lr_1=0.01 
+lr_2=0.001
+lr_3=0.0001
+lr_4=0.00001
 
 def root_path():
     #return os.path.dirname(__file__)
